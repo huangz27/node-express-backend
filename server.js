d452a470e1ef144dfbef99c8aa1ff5fb47fe9ab5
Huang <zwhuang@amazon.com>
2022-04-01 16:57:27 +0800

    revert to port 8080

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
