d779165e977b6a2a53be36f41349b289f781d22c
Huang <zwhuang@amazon.com>
2022-04-01 16:51:27 +0800

    change port to 80

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
