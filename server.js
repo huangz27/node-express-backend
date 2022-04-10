b5bb7f705443064df1476fdb95cacb410dca8d16
Huang <zwhuang@amazon.com>
2022-04-10 11:37:17 +0800

    remove the force resync and drop table

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 const app = express();
 
 const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
 var corsOptions = {
   origin: "http://localhost:8081"
 };
